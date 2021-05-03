e08873bf3c0a45d9b67c52409ef7cd5ab1e7484e
Willie <niles_nelson@me.com>
2021-05-03 18:04:30 -0500

    work

---
+++ josephwork/example.js
@@ -0,0 +1,12 @@
+class Block{
+    constructor(timestamp, data, previousHash = '') {
+      this.previousHash = previousHash;
+      this.timestamp = timestamp;
+      this.data = data;
+    }
+}
+
+calculateHash() {
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+}
+
