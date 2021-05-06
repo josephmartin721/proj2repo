6769d23ba26f9604d0162ba63db3c3d555bdb0a7
josephmartin721 <josephmartin721@yahoo.com>
2021-05-05 19:48:42 -0500

    server on mine

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const eslint = require('eslint')
 const path = require('path');
-const mysql2 = require('mysql2')
 const routes = require('./routes');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-const hbs = exphbs.create({ helpers });
+
+// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session(sess));
+
 app.use(routes);
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+
+// sync sequelize models to the database, then turn on the server
+
+sequelize.sync({force: false}).then(() => {
+
+app.listen(PORT, () =>
+  console.log(`App listening on port ${PORT}!`));
 });
