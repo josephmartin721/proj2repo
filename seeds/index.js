a50bee57637506f511668013d98ed5f64512b016
josephmartin721 <josephmartin721@yahoo.com>
2021-05-08 13:19:11 -0500

    joseph changes

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const seedWallet = require('./wallet-seeds');
+
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
--- seeds/wallet-seeds.js
+++ seeds/wallet-seeds.js
@@ -2,11 +2,12 @@ const { Post } = require('../models');
 
 const postData = [
     {
+        title: 'wallet 1',
         post_content: 'placeholder text values',
     },
     {
-    },
-    {
+        title: 'wallet 2',
+        post_content: 'placeholder text values',
     },
 ]
 
